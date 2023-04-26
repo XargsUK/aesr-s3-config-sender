99993aea6b1d12baffc515439b5dbcb7722a054c
Bradley Duncan <bradley.duncan@ans.co.uk>
2023-04-26 09:22:37 +0100

    init commit

--- js/options.js
+++ js/options.js
@@ -1,25 +1,67 @@
 const elById = (id) => document.getElementById(id);
 
-window.onload = function() {
+window.onload = function () {
   const textArea = elById('awsConfigTextArea');
   const msgSpan = elById('msgSpan');
   const saveButton = elById('saveButton');
+  const pullS3ConfigButton = elById('pullS3ConfigButton');
 
-  saveButton.onclick = function() {
+  saveButton.onclick = function () {
     const aesrSenderId = elById('aesrIdText').value;
 
     chrome.runtime.sendMessage(aesrSenderId, {
       action: 'updateConfig',
       dataType: 'ini',
       data: textArea.value,
-    }, function(response) {
+    }, function (response) {
       if (response) {
         updateMessage(msgSpan, 'Succeeded to send data', '#1111dd');
       } else {
         updateMessage(msgSpan, 'Failed to send data', '#dd1111');
       }
     });
-  }
+  };
+
+  pullS3ConfigButton.onclick = async function () {
+    const accessKeyId = elById('awsAccessKey').value;
+    const secretAccessKey = elById('awsSecretKey').value;
+    const region = elById('awsRegion').value;
+    const bucket = elById('bucketName').value;
+    const key = elById('fileKey').value;
+
+    try {
+      const content = await fetchS3FileContent(accessKeyId, secretAccessKey, region, bucket, key);
+      textArea.value = content;
+      updateMessage(msgSpan, 'Succeeded in fetching S3 file content', '#1111dd');
+    } catch (error) {
+      console.error('Error fetching S3 file content:', error);
+      updateMessage(msgSpan, 'Failed to fetch S3 file content', '#dd1111');
+    }
+  };
+};
+
+function fetchS3FileContent(accessKeyId, secretAccessKey, region, bucket, key) {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(
+      {
+        action: "fetchS3FileContent",
+        accessKeyId,
+        secretAccessKey,
+        region,
+        bucket,
+        key,
+      },
+      (response) => {
+        if (response.success) {
+          console.log("Received S3 file content:", response.content);
+          resolve(response.content);
+        } else {
+          console.error("Failed to fetch S3 file content:", response.error);
+          reject(response.error);
+        }
+      }
+    );
+  });
 }
 
 function updateMessage(el, msg, color) {
