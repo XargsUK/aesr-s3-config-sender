eee8345ac328da578b369e1b102ea3ac93a2efaf
Bradley Duncan <bradley.duncan@ans.co.uk>
2023-05-01 16:45:53 +0100

    add instructions to the bottom. move load profile add title

--- js/initTooltips.js
+++ js/initTooltips.js
@@ -3,3 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.tooltipped');
     var instances = M.Tooltip.init(elems);
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
