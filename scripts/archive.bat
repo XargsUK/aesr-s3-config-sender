51b7fbfed3b5cbf0b62eb36e35a382daca28fb8e
Bradley Duncan <bradley.duncan@ans.co.uk>
2023-05-01 13:39:43 +0100

    fix archive.bat

--- scripts/archive.bat
+++ scripts/archive.bat
@@ -1,9 +1,11 @@
 @echo off
 REM archive.bat
 
+call npm install webpack webpack-cli webpack-dev-server @babel/core @babel/preset-env babel-loader --save-dev
+call npx webpack --config webpack.config.js
 set zipfile=aesr-s3-config-sender.zip
 
-cd dist\chrome
-del dist\%zipfile%
+if not exist dist mkdir dist
+if exist dist\%zipfile% del dist\%zipfile%
 powershell Compress-Archive -Path manifest.json, *.html, icons\, js\, css\ -DestinationPath .\dist\%zipfile%
-echo archived: chrome\%zipfile%
+echo archived: dist\%zipfile%
