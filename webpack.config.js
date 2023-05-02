55679b619f4096b7199005ee0f48aae826dcad11
xargs <51077147+XargsUK@users.noreply.github.com>
2023-05-02 01:14:47 +0100

    webpack materialize npm module

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: {
     options: './src/options.js',
-    materialize: './src/materialize.js'
+    materialize: './node_modules/materialize-css/dist/js/materialize.js'
   },
   output: {
     filename: '[name].bundle.js',
