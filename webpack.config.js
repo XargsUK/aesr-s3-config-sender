0cfcf834a39574604c915049c889923055aa2f16
xargs <51077147+XargsUK@users.noreply.github.com>
2023-05-02 02:26:35 +0100

    Revert "Merge pull request #19 from XargsUK/materialize-update"
    This reverts commit 2d0dc763898a47c88a7dcb53797ae9bf23541bb7, reversing
    changes made to 67144f717456c98754b1e0224657f417248ee54c.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: {
     options: './src/options.js',
-    materialize: './node_modules/materialize-css/dist/js/materialize.js'
+    materialize: './src/materialize.js'
   },
   output: {
     filename: '[name].bundle.js',
