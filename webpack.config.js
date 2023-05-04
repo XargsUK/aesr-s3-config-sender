b24a5972174d5aadb64e2895397785a5a0cf82f7
xargs <51077147+XargsUK@users.noreply.github.com>
2023-05-04 01:10:43 +0100

    adding fa for icons

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,17 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'js'),
+    assetModuleFilename: 'fonts/[name][ext][query]', 
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset/resource',
       },
     ],
   },
